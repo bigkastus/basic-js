0c335cf8383ec9f9a993fa79dad708cf2124e712
bigkastus <bigkastus@gmail.com>
2020-10-27 17:13:08 +0300

    feat: start countcats, end carbondating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,13 +3,28 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(sampleActivity ) {  
-  // remove line with error and write your code here
-  if (sampleActivity === NaN || typeof(parseInt(sampleActivity, 10)) !== 'number' || parseInt(sampleActivity, 10) < 0 ) {
-    return false;
-  }
+module.exports = function dateSample(sampleActivity) {  
   const k = 0.693/HALF_LIFE_PERIOD;
   const result = Math.log(MODERN_ACTIVITY/sampleActivity);
-  const finresult = Math.round(result);  
-  return finresult;
+  const finresult = Math.round(result); 
+  if(typeof(sampleActivity) !== "string"){
+    return false;
+  } 
+  else if (parseInt(sampleActivity, 10) > 0 && 
+      parseInt(sampleActivity, 10) <= 15 ) {
+        if (finresult <= 30) false;
+        return finresult;
+  }
+  
+  return false;  
+  // if (sampleActivity === false || 
+  //     typeof(parseInt(sampleActivity, 10)) !== 'number' || 
+  //     parseInt(sampleActivity, 10) < 0 || 
+  //     parseInt(sampleActivity, 10) > 15 ) {
+  //   return false;
+  // }
+  // const k = 0.693/HALF_LIFE_PERIOD;
+  // const result = Math.log(MODERN_ACTIVITY/sampleActivity);
+  // const finresult = Math.round(result);  
+  // return finresult;
 };
