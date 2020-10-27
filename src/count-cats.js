8ec8850ff0f2bc9274fed79f5fff7df83eca9d1c
bigkastus <bigkastus@gmail.com>
2020-10-27 21:24:41 +0300

    feat: start dteam, end countcats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {  
+  let cats = 0;
+  if (matrix.length === 0) return cats;  
+  for (let i=0; i<matrix.length; i++){
+    let icats = matrix[i].filter(x => x === "^^").length 
+    cats = cats + icats
+  }
+  return cats;
 };
+
+
