20c8dfdb82b3cef5f8f736262b6f9fa3b980691d
bigkastus <bigkastus@gmail.com>
2020-10-28 23:57:04 +0300

    feat; do what season 7 of 8

--- src/what-season.js
+++ src/what-season.js
@@ -1,19 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason( date ) {
+module.exports = function getSeason(date) {
+  // date = new Date();
   if (date === undefined) return 'Unable to determine the time of year!';
-  // date === undefined ? 'Unable to determine the time of year!' : 'ok';
-  if (date[1] < 1 && date[1] >12) {
-    return 'Error'  
+  if (date == false) throw new Error ('Error');
+  // if (date.getMonth() < 0  && date.getMonth() >11 ) {
+  //   throw new Error ('Error');
+  // }
+  else if ( date.getFullYear().length < 4 ){
+    throw new Error ('Error');
   }
-  else if (date[1] > 2 && date[1] < 6 ){
-    return 'spring'
+  else if (date.getMonth() > 1 && date.getMonth() < 5 ){
+    return 'spring';
   }
-  else if (date[1] > 5 && date[1] < 9 ){
+  else if (date.getMonth() > 4 &date.getMonth() < 8 ){
     return 'summer'
   }
-  else if (date[1] > 8 && date[1] < 12 ){
+  else if (date.getMonth() > 7 && date.getMonth() < 11 ){
     return 'autumn'
   }
-  return 'summer'
+  return 'winter'
 };
