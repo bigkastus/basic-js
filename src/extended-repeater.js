feb9d71eca244171603b69a0f617b0d289caa9c2
Kastus <bigkastus@gmail.com>
2020-10-28 15:18:03 +0300

    feat: start what date

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -5,3 +5,9 @@ module.exports = function repeater(/* str, options */) {
   // remove line with error and write your code here
 };
   
+
+// options = { repeatTimes: 1, separator: '+', addition: '|', additionRepeatTimes: 1, additionSeparator: '' }; 
+// str = (str !==  'string') ? str.toString() : str;
+// addition = ('addition' !==  'string') ? 'addition'.toString() : 'addition';
+// str = str.split(options['separator']).push(options['addition']);
+// return str;
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason( date ) {
+  if (date === undefined) return 'Unable to determine the time of year!';
+  // date === undefined ? 'Unable to determine the time of year!' : 'ok';
+  if (date[1] < 1 && date[1] >12) {
+    return 'Error'  
+  }
+  else if (date[1] > 2 && date[1] < 6 ){
+    return 'spring'
+  }
+  else if (date[1] > 5 && date[1] < 9 ){
+    return 'summer'
+  }
+  else if (date[1] > 8 && date[1] < 12 ){
+    return 'autumn'
+  }
+  return 'summer'
 };
